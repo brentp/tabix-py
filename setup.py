ce10b8c50a9e3be6c5287fd841607a775c87d7b3
Brent Pedersen <bpederse@gmail.com>
2014-07-17 11:09:47 -0600

    setuptools. shrug

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import ez_setup
+ez_setup.use_setuptools()
+
 from setuptools import setup
+from glob import glob
 
 import tabix.tabixffi
 import cffi
@@ -9,10 +13,12 @@ cffi.verifier.cleanup_tmpdir()
 
 setup(name='tabix',
       packages=['tabix'],
+      package_data={'tabix': ["tabix/C/"]},
       ext_package='tabixffi',
+      include_package_data=True,
       ext_modules=[ext],
       zip_safe=False,
-      install_requires=['cffi']
+      install_requires=['cffi', 'setuptools>=0.6c11']
       )
 
 import tabix
